9ad98c4cb2caddc994cd5a853d684ade92193649
Dehtiarov-Y <eugene.degtyaryov@gmail.com>
2026-02-14 21:21:49 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -25,7 +25,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const sadImages = [
         "img/1.gif", "img/2.gif", "img/3.gif", 
         "img/4.gif", "img/5.gif", "img/6.gif", 
-        "img/7.gif", "img/8.gif", "img/9.gif"
+        "img/7.gif", "img/8.gif", "img/9.gif",
+        "img/10.gif", "img/11.gif", "img/12.gif",
+        "img/13.gif"
     ];
     let currentImageIndex = 0;
 
